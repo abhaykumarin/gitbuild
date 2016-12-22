e81d7d07e0a04e460916b7b83db3a3cee9e0731b
abhaykumar <abhaykumar@192.168.0.2>
2016-12-22 14:25:05 +0530

    Commit by Abhay 1

---
+++ gitbuilddemo/src/gitbuildtest/PrintDeom.java
@@ -0,0 +1,10 @@
+package gitbuildtest;
+
+public class PrintDeom {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("This is a Jenkind build demo 2");
+	}
+
+}
